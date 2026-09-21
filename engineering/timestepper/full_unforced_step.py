670bd973eea72ebf0584677a08f7f79fab1c9ff9
Morgan Sherer <163759418+AlkaiDynamics@users.noreply.github.com>
2026-09-21 15:29:20 -0700

    fix: split Real gauge block from full timestep Jacobian

--- engineering/timestepper/full_unforced_step.py
+++ engineering/timestepper/full_unforced_step.py
@@ -284,8 +284,27 @@ def one_step(dt_value=2.0e-3):
             "snes_atol": 1.0e-12,
             "snes_stol": 1.0e-12,
             "snes_max_it": 40,
-            "ksp_type": "preonly",
-            "pc_type": "lu",
+            "mat_type": "matfree",
+            "ksp_type": "fgmres",
+            "ksp_rtol": 1.0e-11,
+            "pc_type": "fieldsplit",
+            "pc_fieldsplit_type": "schur",
+            "pc_fieldsplit_schur_fact_type": "full",
+            "pc_fieldsplit_0_fields": "0,1,2,3,4,5,6",
+            "pc_fieldsplit_1_fields": "7",
+            "fieldsplit_0": {
+                "ksp_type": "preonly",
+                "pc_type": "python",
+                "pc_python_type": "firedrake.AssembledPC",
+                "assembled": {
+                    "ksp_type": "preonly",
+                    "pc_type": "lu",
+                },
+            },
+            "fieldsplit_1": {
+                "ksp_type": "preonly",
+                "pc_type": "none",
+            },
         },
     )
 
