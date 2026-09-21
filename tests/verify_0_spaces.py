0bbffc0a5f146ad7e80e861495e2d75ba44939e2
Morgan Sherer <163759418+AlkaiDynamics@users.noreply.github.com>
2026-09-21 11:40:34 -0700

    numerics: test reference exterior derivative with BDFM Piola map

--- tests/verify_0_spaces.py
+++ tests/verify_0_spaces.py
@@ -36,6 +36,14 @@ from firedrake import (
 )
 from gusto.core.domain import Domain
 from petsc4py import PETSc
+from ufl import (
+    Jacobian,
+    JacobianDeterminant,
+    ReferenceGrad,
+    ReferenceValue,
+    as_vector,
+    dot,
+)
 
 
 GUSTO_PINNED_COMMIT = "669f6372cd334ed47c9c7b38f26e591732273f75"
